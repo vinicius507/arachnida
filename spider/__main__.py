e8be467d081422ef6657421d118e79aee812e66c
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-18 17:18:44 -0300

    feat(spider): Removes forbidden libraries

--- spider/__main__.py
+++ spider/__main__.py
@@ -1,9 +1,31 @@
-from spider.settings import SpiderSettings
+from argparse import ArgumentParser, Namespace
+from urllib.parse import urlparse
+
+
+class URL(str):
+    def __new__(cls, url: str) -> "URL":
+        if not urlparse(url).scheme:
+            raise ValueError(f"Invalid URL: {url}")
+        return super().__new__(cls, url)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+    def __repr__(self) -> str:
+        return f"URL({super().__repr__()})"
+
+
+class SpiderNamespace(Namespace):
+    url: URL
 
 
 def main() -> None:
-    settings = SpiderSettings()
-    print(f"{settings=}")
+    parser = ArgumentParser(description="Extracts images from a given URL.")
+
+    parser.add_argument("url", type=URL, help="The URL to extract images from")
+    args = parser.parse_args(None, SpiderNamespace())
+
+    print(args)
 
 
 if __name__ == "__main__":
