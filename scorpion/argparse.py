6fa97d38192082059c73e5024fb6b8d747821df6
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-07-21 15:45:43 -0300

    fix(scorpion): Adds help text to files parameter

--- scorpion/argparse.py
+++ scorpion/argparse.py
@@ -13,5 +13,5 @@ def parse_args() -> ScorpionNamespace:
         description="Handle images metadata with ease.",
         epilog="École 42 project by vgoncalv.",
     )
-    parser.add_argument("files", nargs="+", type=Image)
+    parser.add_argument("files", nargs="+", type=Image, help="Image files to handle.")
     return parser.parse_args(namespace=ScorpionNamespace())
